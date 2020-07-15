79e33ad631b7931d82df17b10de59cb87bf9a479
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2020-07-15 16:18:36 -0500

    Add final files

---
+++ app/js/script.js
@@ -0,0 +1,28 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');
+
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
+});
